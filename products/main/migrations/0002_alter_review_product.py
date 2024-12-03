e1a475f15456f2784424ceacbab49d1a38e80d3e
Neto test <atlantic92@mail.ru>
2024-12-03 22:35:35 +0300

    first commit

---
+++ products/main/migrations/0002_alter_review_product.py
@@ -0,0 +1,17 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='review',
+            name='product',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='reviews', to='main.product'),
+        ),
+    ]
